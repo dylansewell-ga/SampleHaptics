d227e0404bf5590c0a0fd394949d4f7e538ac769
Dylan Sewell <dylan.sewell@getaround.com>
2019-11-12 20:34:36 -0800

    Initial Commit

---
+++ SampleHaptics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleHaptics
+//
+//  Created by Dylan Sewell on 11/12/19.
+//  Copyright © 2019 Dylan Sewell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
